410b7f455156f5fec2ebb4cbbeebddac8c41d593
Felipe Borba <felipe.borba0109@gmail.com>
2024-03-23 18:26:39 -0300

    feat: add Top bar

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,5 +1,10 @@
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.transitions.SlideTransition
 import org.jetbrains.compose.resources.ExperimentalResourceApi
@@ -11,7 +16,14 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 fun App() {
     MaterialTheme {
         Navigator(ScreenA()) { navigator ->
-            SlideTransition(navigator)
+            Scaffold(
+                topBar = { TopAppBar { Text("Voyager") } }
+            ) { innerPadding ->
+                SlideTransition(
+                    navigator = navigator,
+                    modifier = Modifier.padding(innerPadding)
+                )
+            }
         }
     }
 }
