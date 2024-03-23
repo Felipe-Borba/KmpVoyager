d651c5630ca0cd9ee69f6484359d3121cf3476c1
Felipe Borba <felipe.borba0109@gmail.com>
2024-03-23 19:11:49 -0300

    add tab navigation

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,4 +1,8 @@
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
@@ -6,6 +10,10 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.navigator.tab.CurrentTab
+import cafe.adriel.voyager.navigator.tab.LocalTabNavigator
+import cafe.adriel.voyager.navigator.tab.Tab
+import cafe.adriel.voyager.navigator.tab.TabNavigator
 import cafe.adriel.voyager.transitions.SlideTransition
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -15,15 +23,46 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 fun App() {
     MaterialTheme {
-        Navigator(ScreenA()) { navigator ->
+//        Navigator(ScreenA()) { navigator ->
+//            Scaffold(
+//                topBar = { TopAppBar { Text("Voyager") } }
+//            ) { innerPadding ->
+//                SlideTransition(
+//                    navigator = navigator,
+//                    modifier = Modifier.padding(innerPadding)
+//                )
+//            }
+//        }
+        TabNavigator(TabC) {
             Scaffold(
-                topBar = { TopAppBar { Text("Voyager") } }
-            ) { innerPadding ->
-                SlideTransition(
-                    navigator = navigator,
-                    modifier = Modifier.padding(innerPadding)
+                content = {
+                    CurrentTab()
+                },
+                bottomBar = {
+                    BottomNavigation {
+                        TabNavigationItem(TabC)
+                        TabNavigationItem(TabD)
+                    }
+                }
+            )
+        }
+    }
+}
+
+@Composable
+private fun RowScope.TabNavigationItem(tab: Tab) {
+    val tabNavigator = LocalTabNavigator.current
+
+    BottomNavigationItem(
+        selected = tabNavigator.current == tab,
+        onClick = { tabNavigator.current = tab },
+        icon = {
+            tab.options.icon?.let {
+                Icon(
+                    painter = it,
+                    contentDescription = tab.options.title
                 )
             }
         }
-    }
+    )
 }
---
+++ composeApp/src/commonMain/kotlin/TabD.kt
@@ -0,0 +1,35 @@
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import cafe.adriel.voyager.navigator.tab.Tab
+import cafe.adriel.voyager.navigator.tab.TabOptions
+
+object TabD : Tab {
+    @Composable
+    override fun Content() {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text("Tab D")
+        }
+    }
+
+    override val options: TabOptions
+        @Composable
+        get() {
+            val icon = rememberVectorPainter(Icons.Default.Settings)
+            val title = "Tab D"
+            val index: UShort = 1u
+
+            return TabOptions(index, title, icon)
+        }
+}
